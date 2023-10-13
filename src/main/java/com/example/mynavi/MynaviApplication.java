35b80cdb088d0cb68e4c65cdb98aba9d6406afcf
강현욱 <bug6868@naver.com>
2023-10-13 10:17:09 +0900

    Initial commit

---
+++ src/main/java/com/example/mynavi/MynaviApplication.java
@@ -0,0 +1,13 @@
+package com.example.mynavi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MynaviApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MynaviApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mynavi/MynaviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mynavi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MynaviApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
